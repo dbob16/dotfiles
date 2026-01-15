1755c82a9604480c4bcd491974065bb0fa4c8e67
Dilan Gilluly <gillulyd@gmail.com>
2026-01-14 20:08:58 -0500

    (fix): av and cv functions

--- .config/dg-bash/functions.sh
+++ .config/dg-bash/functions.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function cv() {
-  mkdir -p ~/python-venv
-  python -m venv "~/python-venv/$1"
-  source "~/python-venv/$1/bin/activate"
+  mkdir -p $HOME/python-venv
+  python -m venv "$HOME/python-venv/$1"
+  source "$HOME/python-venv/$1/bin/activate"
 }
 
 function av() {
-  if [ -d "~/python-venv/$1" ]; then
-    source "~/python-venv/$1/bin/activate"
+  if [ -d "$HOME/python-venv/$1" ]; then
+    source "$HOME/python-venv/$1/bin/activate"
   else
     echo "Error: Venv does not exist"
   fi
